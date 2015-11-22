051e3c7233a87f2b0f6810ba9a5d06eec22a020f
Jordan S Read <jread@usgs.gov>
2015-11-22 07:23:40 -0600

    clean up

--- R/render_svg.R
+++ R/render_svg.R
@@ -18,9 +18,9 @@ svg <- function(object, ...){
 svg.gsplot <- function(object, file = "Rplot.svg", width = 6, height = 4.3, pointsize = 12, ...){
 
   par <- par(object)
-  svg <- init_svg(width, height, ...) %>% 
-    svg_window(window=object$view$window) %>% 
-    svg_points(window=object$view$window, points=object$view$points)
+  svg <- init_svg(width, height, ...)
+  svg_window(svg, window=object$view$window)
+  svg_points(svg, window=object$view$window, points=object$view$points)
   # get par
   # set the page dimensions
   # translate coordinates
@@ -43,23 +43,27 @@ svg_view_bounds <- function(svg, mai=par()$mai){
          height = view.box[4]-mar[1]-mar[3])
 }
 
+
 #' @importFrom XML newXMLTextNode
 svg_window <- function(svg, window){
+
+  g.view <- svg_node('g', svg, c('id'=sprintf('view-%s-%s', window$side[1], window$side[2])))
+  g.axes <- svg_node('g', g.view, c('id'="axes", 'fill'="none", 'stroke'="#000000", 'stroke-width'="1"))
   
   ax <- svg_view_bounds(svg)
+  svg_node('rect', g.axes, c(x=ax[['x']], y=ax[['y']], height=ax[['height']], width=ax[['width']], id='axes-box'))
+  
+  
   tick.len <- 5
-  g.axes <- newXMLNode('g', parent = svg,
-                    attrs=c(id='axes', fill="none", stroke="#000000", 'stroke-width'="1"))
   
-  
-  
-  newXMLNode('rect', parent = g.axes,
-             attrs=c(x=ax[['x']], y=ax[['y']], height=ax[['height']], width=ax[['width']], id='axes-box'))
-  
+  x.p <- pretty(window$xlim)
+  y.p <- pretty(window$ylim)
+  coord.tick <- list(x = x.p[x.p >= min(window$xlim) & x.p <= max(window$xlim)],
+                     y = y.p[y.p >= min(window$ylim) & y.p <= max(window$ylim)])
   # x-ticks
   tick.loc <- list(
-    x = svg_coords(pretty(window$xlim), window$xlim, c(ax[['x']], ax[['x']] + ax[['width']]), return.out=T),
-    y = svg_coords(pretty(window$ylim), window$ylim, c(ax[['y']] + ax[['height']], ax[['y']]), return.out=T))
+    x = svg_coords(coord.tick$x, window$xlim, c(ax[['x']], ax[['x']] + ax[['width']]), return.out=T),
+    y = svg_coords(coord.tick$x, window$ylim, c(ax[['y']] + ax[['height']], ax[['y']]), return.out=T))
   
   x.paths <- paste0('M', paste(paste(as.vector(sapply(tick.loc$x, rep, 2)),
                                      c(ax[['y']] + ax[['height']],ax[['y']] + ax[['height']] - tick.len), sep=','),c('L','M'), collapse=''))
@@ -72,22 +76,26 @@ svg_window <- function(svg, window){
   
   newXMLNode('path', parent = x.axis,
              attrs=c(d=x.paths, id='x-axis-ticks'))
-  for (i in seq_len(length(tick.loc$x))){
-    newXMLNode("text", newXMLTextNode(pretty(window$xlim)[i]), 'parent' = x.axis,
-               attrs=c(x=tick.loc$x[i], y=ax[['y']] + ax[['height']], dy='1.0em', stroke='none',fill='#000000', 'text-anchor'="middle"))
+  x.axis.text <- newXMLNode('g', parent = x.axis, attrs=c(id='x-axis-text', stroke='none',fill='#000000', 'text-anchor'="middle"))
+  for (i in seq_len(length(coord.tick$x))){
+    newXMLNode("text", newXMLTextNode(coord.tick$x[i]), 'parent' = x.axis.text,
+               attrs=c(x=tick.loc$x[i], y=ax[['y']] + ax[['height']], dy='1.0em'))
   }
   
   newXMLNode('path', parent = y.axis,
              attrs=c(d=y.paths, id='y-axis-ticks'))
-  
-  for (i in seq_len(length(tick.loc$y))){
-    newXMLNode("text", newXMLTextNode(pretty(window$ylim)[i]), 'parent' = y.axis,
-               attrs=c(y=tick.loc$y[i], x=ax[['x']], dx='-0.33em', dy='0.33em', stroke='none',fill='#000000', 'text-anchor'="end"))
+  y.axis.text <- newXMLNode('g', parent = y.axis, attrs=c(id='y-axis-text', stroke='none',fill='#000000', 'text-anchor'="end"))
+  for (i in seq_len(length(coord.tick$y))){
+    newXMLNode("text", newXMLTextNode(coord.tick$y[i]), 'parent' = y.axis.text,
+               attrs=c(y=tick.loc$y[i], x=ax[['x']], dx='-0.33em', dy='0.33em'))
   }
   invisible(svg)
 }
 
-
+svg_node <- function(name, parent, attrs, ...){
+  invisible(newXMLNode(name = name, parent = parent,
+             attrs=attrs, ...))
+}
 
 #' points to svg
 #' 
