b4d227827674aa460415f41329ecf8df79c511f7
Jordan S Read <jread@usgs.gov>
2015-11-24 16:06:44 -0600

    WIP for markers pch render_pts

--- R/render_points.R
+++ R/render_points.R
@@ -2,7 +2,7 @@ render_points <- function(g.view, x, y, pch=par("pch"), col=par("col"), bg="#FFF
   
   args <- expand.grid(..., stringsAsFactors = FALSE)
   view.bounds <- view_bounds(g.view)
-  radius <- as.crd(cex*2.7) # need to use ppi?
+  
   coords <- svg_coords(x, y, xlim, ylim, view.bounds)
   
   clip.id <- svg_id(g_mask(g.view))
@@ -11,6 +11,7 @@ render_points <- function(g.view, x, y, pch=par("pch"), col=par("col"), bg="#FFF
     hovertext <- rep(hovertext, length(coords$x))
   }
   
+  pch <- as.character(pch)
   g.geom <- svg_node('g', g.view, c('fill'=col, 'clip-path'=sprintf("url(#%s)",clip.id), args))
   
   for (i in seq_len(length(coords$x))){
@@ -18,10 +19,76 @@ render_points <- function(g.view, x, y, pch=par("pch"), col=par("col"), bg="#FFF
       hover.args <- c(onmouseover=sprintf("hovertext('%s',%s,%s)",hovertext[i],coords$x[i],coords$y[i]), onmouseout="hovertext(' ')") 
     } else 
       hover.args <- NULL
-    svg_node('circle', g.geom, c(cx=coords$x[i], cy=coords$y[i], r=radius, hover.args))
+    points_node(g.geom, coords$x[i], coords$y[i], pch, col, bg, cex, lwd, hover.args)
   }
 }
 
-points_node <- function(g, x, y, pch, col, bg, cex, lwd){
+as.path <- function(x,y){
+  paste0('M ',paste(x,y,sep=',',collapse=' L'), 'Z')
+}
+points_node <- function(g, x, y, pch, col, bg, cex, lwd, ...){
+  
+  num <- as.numeric(c(x,y)) # hack! should still be numeric. Need to make it as.crd as the *last* step when entering node. This is slow I bet
+  std.rad <- 2.7 #px
+  geoms <- list(circle = c(cx=x, cy=y, r=as.crd(cex*std.rad)),
+                sm.circle = c(cx=x, cy=y, r=as.crd(cex*std.rad*0.7)),
+                square = c(d=as.path(x=c(num[1]-std.rad,num[1]+std.rad,num[1]+std.rad,num[1]-std.rad), 
+                                     y=c(num[2]-std.rad,num[2]-std.rad,num[2]+std.rad,num[2]+std.rad))),
+                up.tri = c(d=as.path(x=c(num[1],num[1]+std.rad*1.125,num[1]-std.rad*1.125), 
+                                       y=c(num[2]-std.rad*1.5,num[2]+std.rad*0.75,num[2]+std.rad*0.75))),
+                diamond = c(d=as.path(x=c(num[1],num[1]+std.rad*1.41,num[1],num[1]-std.rad*1.41), 
+                                      y=c(num[2]-std.rad*1.41,num[2],num[2]+std.rad*1.41,num[2]))),
+                cross = c(d=paste(as.path(x=c(num[1],num[1]),y=c(num[2]-std.rad*1.41,num[2]+std.rad*1.41)),
+                          as.path(x=c(num[1]+std.rad*1.41, num[1]-std.rad*1.41),y=c(num[2], num[2])), sep=' ')),
+                sm.cross = c(d=paste(as.path(x=c(num[1],num[1]),y=c(num[2]-std.rad,num[2]+std.rad)),
+                                  as.path(x=c(num[1]+std.rad, num[1]-std.rad),y=c(num[2], num[2])), sep=' ')),
+                x = c(d=paste(as.path(x=c(num[1]-std.rad,num[1]+std.rad), y=c(num[2]-std.rad,num[2]+std.rad)), 
+                              as.path(x=c(num[1]+std.rad,num[1]-std.rad), y=c(num[2]-std.rad,num[2]+std.rad)), sep=' ')),
+                dwn.tri = c(d=as.path(x=c(num[1],num[1]+std.rad*1.125,num[1]-std.rad*1.125), 
+                                      y=c(num[2]+std.rad*1.5,num[2]-std.rad*0.75,num[2]-std.rad*0.75))),
+                tri.square = c(d=paste(as.path(x=c(num[1]-std.rad,num[1]+std.rad,num[1]+std.rad,num[1]-std.rad), 
+                                        y=c(num[2]-std.rad,num[2]-std.rad,num[2]+std.rad,num[2]+std.rad)),
+                                as.path(x=c(num[1],num[1]-std.rad,num[1]+std.rad), y=c(num[2]-std.rad,num[2]+std.rad+num[2]-std.rad)),sep=' ')))
+                
+  
+  transparent <- "#FFFFFF"
   # here, support multiple pch values w/ a switch
+  node <- switch(pch,
+          '0' = svg_node('path', g, c(geoms[['square']], fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '1' = svg_node('circle', g, c(geoms[['circle']], fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '2' = svg_node('path', g, c(geoms[['up.tri']], fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '3' = svg_node('path', g, c(geoms[['cross']], fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '4' = svg_node('path', g, c(geoms[['x']], fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '5' = svg_node('path', g, c(geoms[['diamond']], fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '6' = svg_node('path', g, c(geoms[['dwn.tri']], fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '7' = svg_node('path', g, c(d=paste0(geoms[['square']][['d']],' ',geoms[['x']][['d']]), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '8' = svg_node('path', g, c(d=paste0(geoms[['cross']][['d']],' ',geoms[['x']][['d']]), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '9' = svg_node('path', g, c(d=paste0(geoms[['cross']][['d']],' ',geoms[['diamond']][['d']]), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '10' = svg_node('path', g, c(d=paste0(geoms[['sm.cross']][['d']],' ',geoms[['diamond']][['d']]), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '11' = svg_node('path', g, c(d=paste0(geoms[['up.tri']][['d']],' ',geoms[['dwn.tri']][['d']]), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '12' = svg_node('path', g, c(d=paste0(geoms[['sm.cross']][['d']],' ',geoms[['square']][['d']]), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '13' = svg_node('path', g, c(d=paste0(geoms[['x']][['d']],' ',geoms[['square']][['d']]), fill=transparent, 'fill-opacity'="0", stroke=col, ...)), #needs path circle
+          '14' = svg_node('path', g, c(geoms[['tri.square']], fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '15' = svg_node('path', g, c(geoms[['square']], fill=col, stroke=col, ...)),
+          '16' = svg_node('circle', g, c(cx=x, cy=y, r=as.crd(cex*2.7), fill=col, stroke=col, ...)),
+          '17' = svg_node('path', g, c(geoms[['up.tri']], fill=col, stroke=col, ...)),
+          '18' = svg_node('path', g, c(geoms[['diamond']], fill=col, stroke=col, ...)),
+          '19' = svg_node('circle', g, c(geoms[['circle']], fill=col, stroke=col, ...)),
+          '20' = svg_node('circle', g, c(geoms[['sm.circle']], fill=col, stroke=col, ...)),
+          '21' = svg_node('circle', g, c(geoms[['circle']], fill=bg, stroke=col, ...)),
+          '22' = svg_node('path', g, c(geoms[['square']], fill=bg, stroke=col, ...)),
+          '23' = svg_node('circle', g, c(cx=x, cy=y, r=as.crd(cex*2.7), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '24' = svg_node('circle', g, c(cx=x, cy=y, r=as.crd(cex*2.7), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '25' = svg_node('circle', g, c(cx=x, cy=y, r=as.crd(cex*2.7), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '.' = svg_node('circle', g, c(cx=x, cy=y, r=as.crd(cex*2.7), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          'o' = svg_node('circle', g, c(cx=x, cy=y, r=as.crd(cex*2.7), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '0' = svg_node('circle', g, c(cx=x, cy=y, r=as.crd(cex*2.7), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          'O' = svg_node('circle', g, c(cx=x, cy=y, r=as.crd(cex*2.7), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          'a' = svg_node('circle', g, c(cx=x, cy=y, r=as.crd(cex*2.7), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          'A' = svg_node('circle', g, c(cx=x, cy=y, r=as.crd(cex*2.7), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '*' = svg_node('circle', g, c(cx=x, cy=y, r=as.crd(cex*2.7), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '+' = svg_node('circle', g, c(cx=x, cy=y, r=as.crd(cex*2.7), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '-' = svg_node('circle', g, c(cx=x, cy=y, r=as.crd(cex*2.7), fill=transparent, 'fill-opacity'="0", stroke=col, ...)),
+          '|' = svg_node('circle', g, c(cx=x, cy=y, r=as.crd(cex*2.7), fill=transparent, 'fill-opacity'="0", stroke=col, ...)))
+  invisible(node)
 }
