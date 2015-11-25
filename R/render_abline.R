72111a791cc0fa29ef36c32ccfd9fc9dfdb71269
Jordan S Read <jread@usgs.gov>
2015-11-25 15:18:13 -0600

    simpler

--- R/render_abline.R
+++ R/render_abline.R
@@ -7,20 +7,16 @@ render_abline <- function(g.view, a = NULL, b = NULL, h = NULL, v = NULL, reg = 
   g.geom <- svg_node('g', g.view, c('stroke'=as.rgb(col), as.lty(lty), 'clip-path'=sprintf("url(#%s)",clip.id), args))
   
   if (!is.null(v)){
-    y1 <- view.bounds[['y']] + view.bounds[['height']]
-    y2 <- view.bounds[['y']]
     
     x <- svg_coords(x=v,xlim=xlim, view.bounds = view.bounds)$x
     for (i in seq_len(length(x))){
-      svg_node('path', g.geom, c(d=sprintf('M %s,%s V %s',x[i], y1, y2)))
+      svg_node('path', g.geom, c(d=sprintf('M %s,%s v %s',x[i], view.bounds[['y']], view.bounds[['height']])))
     }
   } else if (!is.null(v)){
-    x1 <- view.bounds[['x']]
-    x2 <- view.bounds[['x']] + view.bounds[['width']]
     
     y <- svg_coords(y=h,ylim=ylim, view.bounds = view.bounds)$y
     for (i in seq_len(length(y))){
-      svg_node('path', g.geom, c(d=sprintf('M %s,%s H %s',x1, y[i], y2)))
+      svg_node('path', g.geom, c(d=sprintf('M %s,%s h %s',view.bounds[['x']], y[i], view.bounds[['width']])))
     }
   } else {
     message('these arguments are not currently supported for render_abline')
