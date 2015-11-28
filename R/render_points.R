e94dbf177ee1cb787aafeb6438b05fd200a0cf49
Jordan S Read <jread@usgs.gov>
2015-11-28 07:48:43 -0600

    bug fix pch 14

--- R/render_points.R
+++ R/render_points.R
@@ -47,8 +47,8 @@ points_node <- function(g, x, y, pch, col, bg, cex, lwd, ...){
                 dwn.tri = c(d=as.path(x=c(num[1],num[1]+std.rad*1.35,num[1]-std.rad*1.35), 
                                       y=c(num[2]+std.rad*1.5,num[2]-std.rad*0.75,num[2]-std.rad*0.75))),
                 tri.square = c(d=sprintf('M %s,%s h %s v %s h %s Z M %s,%s L %s,%s L %s,%s', 
-                                         num[1]-std.rad,num[2]+std.rad, 2*std.rad, 2*std.rad, -2*std.rad, 
-                                         num[1]-std.rad,num[2]+std.rad, num[1], num[2]+3*std.rad, num[1]+std.rad, num[2]+std.rad)))
+                                         num[1]-std.rad,num[2]-std.rad, 2*std.rad, 2*std.rad, -2*std.rad, 
+                                         num[1]-std.rad,num[2]-std.rad, num[1], num[2]+std.rad, num[1]+std.rad, num[2]-std.rad)))
                 
   
   transparent <- "#FFFFFF"
