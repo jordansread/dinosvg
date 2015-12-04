bebe8d9891d4f26418bf4813f85fefb51d8f40fc
Jordan S Read <jread@usgs.gov>
2015-12-04 15:23:30 -0600

    clean up for API breaking change

--- R/transform_pt.R
+++ R/transform_pt.R
@@ -1,29 +1,3 @@
-tran_x <- function(val, axes, fig){
-  
-  plt_rng <- diff(axes$x_lim)
-  px_rng = diff(fig$px_lim$x)
-  px_rat <- px_rng/plt_rng # ratio of px per plot unit val
-  
-  x_px <- fig$px_lim$x[1]+(val-axes$x_lim[1])*px_rat
-  
-  # gets value and bounds of x, returns "pixel-space" value for x
-
-  return(x_px)
-  
-}
-tran_y <- function(val, axes, fig){
-
-  plt_rng <- diff(axes$y_lim)
-  px_rng = diff(rev(fig$px_lim$y))
-  px_rat <- px_rng/plt_rng # ratio of px per log unit val
-  
-  y_px <- fig$px_lim$y[1]-(val-axes$y_lim[1])*px_rat
-  
-  # gets value and bounds of x, returns "pixel-space" value for x
-  
-  return(y_px)
-  
-}
 
 svg_coords <- function(x, y=NULL, xlim, ylim=NULL, view.bounds, log='', as.crd=TRUE){
   coords <- list()
