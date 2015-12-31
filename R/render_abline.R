fae8afb1de27ddd46bf97606234ac0c2a73efb6a
Jordan S Read <jread@usgs.gov>
2015-12-31 11:20:38 -0600

    clean up for abline h&v args

--- R/render_abline.R
+++ R/render_abline.R
@@ -1,22 +1,37 @@
+#' create abline svg elements
+#' 
+#' creates abline elements using the syntax of \code{\link[graphics]{abline}}
+#' 
+#' @examples 
+#' \dontrun{
+#' library(gsplot)
+#' gs <- gsplot() %>% 
+#'    points(y=1:11, x=1:11, 
+#'             col="blue", pch=18, xlab='pizza', ylab='dogs') %>% 
+#'    abline(h=1:4)
+#' svg(gs)
+#' }
+#' @export
 render_abline <- function(g.view, a = NULL, b = NULL, h = NULL, v = NULL, reg = NULL, coef = NULL, untf = FALSE, 
                           lty=par("lty"), col=par("col"), lwd=par("lwd"), xlim, ylim, ...){
   
-  args <- expand.grid(..., stringsAsFactors = FALSE)
+  args <- filter_dot_args(...)
   view.bounds <- view_bounds(g.view)
   clip.id <- svg_id(g_mask(g.view))
-  g.geom <- svg_node('g', g.view, c('stroke'=as.rgb(col), as.lty(lty), 'clip-path'=sprintf("url(#%s)",clip.id), args))
+  g.geom <- svg_node('g', g.view, c('stroke'=as.rgb(col), as.lty(lty), 'clip-path'=sprintf("url(#%s)",clip.id), g_args(args)))
   
   if (!is.null(v)){
     
     x <- svg_coords(x=v,xlim=xlim, view.bounds = view.bounds)$x
     for (i in seq_len(length(x))){
-      svg_node('path', g.geom, c(d=sprintf('M %s,%s v %s',x[i], view.bounds[['y']], view.bounds[['height']])))
+      svg_node('path', g.geom, c(d=sprintf('M %s,%s v %s',x[i], view.bounds[['y']], view.bounds[['height']]), nd_args(args,i)))
     }
-  } else if (!is.null(v)){
+  } 
+  if (!is.null(h)){
     
     y <- svg_coords(y=h,ylim=ylim, view.bounds = view.bounds)$y
     for (i in seq_len(length(y))){
-      svg_node('path', g.geom, c(d=sprintf('M %s,%s h %s',view.bounds[['x']], y[i], view.bounds[['width']])))
+      svg_node('path', g.geom, c(d=sprintf('M %s,%s h %s',view.bounds[['x']], y[i], view.bounds[['width']]), nd_args(args,i)))
     }
   } else {
     message('these arguments are not currently supported for render_abline')
