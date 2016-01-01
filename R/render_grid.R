47a9dfbf97ccac4a3a4a8a0be935261c6b51d7b2
Jordan S Read <jread@usgs.gov>
2016-01-01 11:06:47 -0600

    first take on #6

--- R/render_grid.R
+++ R/render_grid.R
@@ -1,14 +1,14 @@
 render_grid <- function(g.view, nx = NULL, ny = nx, col = "lightgray", lty = "dotted", lwd = par("lwd"), equilogs = TRUE, xlim, ylim, ...){
-  args <- expand.grid(..., stringsAsFactors = FALSE)
+  args <- filter_dot_args(...)
   view.bounds <- view_bounds(g.view)
   clip.id <- svg_id(g_mask(g.view))
-  g.geom <- svg_node('g', g.view, c('stroke'=as.rgb(col), as.lty(lty), 'clip-path'=sprintf("url(#%s)",clip.id), args))
+  g.geom <- svg_node('g', g.view, c('stroke'=as.rgb(col), as.lty(lty), 'clip-path'=sprintf("url(#%s)",clip.id), g_args(args)))
   
   # x
   if (is.null(nx)){
     x <- tick_location(g.view, 'x')
     for (i in seq_len(length(x))){
-      svg_node('path', g.geom, c(d=sprintf('M %s,%s v %s',x[i], view.bounds[['y']], view.bounds[['height']])))
+      svg_node('path', g.geom, c(d=sprintf('M %s,%s v %s',x[i], view.bounds[['y']], view.bounds[['height']]), nd_args(args)))
     }
   } else if (!is.na(nx) && nx > 0){
     message('these arguments are not currently supported for render_grid')
@@ -16,10 +16,9 @@ render_grid <- function(g.view, nx = NULL, ny = nx, col = "lightgray", lty = "do
   
   # y
   if (is.null(ny)){
-    y <- tick_location(g.view, 'y')
-    for (i in seq_len(length(y))){
-      svg_node('path', g.geom, c(d=sprintf('M %s,%s h %s',view.bounds[['x']], y[i], view.bounds[['width']])))
-    }
+    addChildren(g.geom, kids=lapply(tick_location(g.view, 'y'), function(x) {
+      newXMLNode('path', attrs=c(d=sprintf('M %s,%s h %s',view.bounds[['x']], x, view.bounds[['width']]), nd_args(args)))
+      }))
   } else if (!is.na(ny) && ny > 0){
     message('these arguments are not currently supported for render_grid')
   }
